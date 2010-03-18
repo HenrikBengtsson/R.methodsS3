5d12c9c9df05a21c22a4a534315ef8015bbc1f76
Henrik Bengtsson <hb@aroma-project.org>
2010-03-18 13:43:04 +0000

    o CLEAN UP: Removed non-needed/non-exported .conflicts.OK.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.methodsS3@351 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,3 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-.conflicts.OK <- TRUE 
-
 ## .First.lib <- function(libname, pkgname) {
 .onAttach <- function(libname, pkgname) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
