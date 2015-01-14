d8a646df5ef38785de263300614b7ff8e69713c9
hb <hb@aroma-project.org>
2015-01-14 09:30:23 -0800

    More testing

--- tests/getDispatchMethodS3.R
+++ tests/getDispatchMethodS3.R
@@ -9,3 +9,10 @@ tryCatch({
 }, error = function(ex) {
   print(ex)
 })
+
+
+## Odds and ends
+# Trying to retrieve base::.Options, but should be
+# detected as a non-function and return an empty result
+fcn <- findDispatchMethodsS3("", "Options")
+stopifnot(length(fcn) == 0L)
