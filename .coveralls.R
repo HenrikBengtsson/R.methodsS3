efe2639ebbeb3626bf887684c9d6f6587ada4f15
hb <hb@aroma-project.org>
2015-01-23 13:54:41 -0800

    Coveralls: Fix.

--- .coveralls.R
+++ .coveralls.R
@@ -12,26 +12,45 @@ requireNamespace("covr") || stop("Package not loaded: covr")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Local functions
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-excl_files <- function(files, max_lines=10e3) {
-  excl <- lapply(files, FUN=function(...) seq_len(max_lines))
+r_files <- function() {
+  dir(path="R", pattern="[.]R$", full.names=TRUE)
+}
+
+excl_files <- function(files, ..., max_lines=10e3) {
+  excl <- lapply(c(files, ...), FUN=function(...) seq_len(max_lines))
   names(excl) <- files
   excl
 }
+
 stop_lines <- function(file) {
   grep("(^|[ \t])(abort|stop|throw)[(]", readLines(file))
 }
+
+no_covr_lines <- function(file) {
+  grep("#[ ]*covr:no", readLines(file))
+}
+
+add_excl <- function(excl=list(), new) {
+  for (name in names(new)) {
+    excl[[name]] <- unique(sort(c(excl[[name]], new[[name]])))
+  }
+  excl
+}
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Exclusion rules
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 excl <- list()
 
-## Don't report coverage on certain files
-excl <- c(excl, excl_files("R/zzz.R"))
-
 ## Don't report on stop() lines
-files <- dir(path="R", pattern="[.]R$", full.names=TRUE)
-excl <- c(excl, sapply(files, FUN=stop_lines))
+excl <- add_excl(excl, sapply(r_files(), FUN=stop_lines))
+
+## Don't report on lines marked with # covr:no
+excl <- add_excl(excl, sapply(r_files(), FUN=no_covr_lines))
+
+## Don't report on certain files
+excl <- add_excl(excl, excl_files("R/zzz.R"))
 
 ## Debugging
 str(excl)
@@ -40,4 +59,9 @@ str(excl)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Run through tests and record source code coverage
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-covr::coveralls(exclusions=excl)
+if (interactive()) {
+  coverage <- covr::package_coverage(exclusions=excl)
+  print(coverage)
+} else {
+  covr::coveralls(exclusions=excl)
+}
