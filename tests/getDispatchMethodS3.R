01b0967c66ad931761c9602a96e45bbc2b3673e1
hb <hb@aroma-project.org>
2015-02-02 19:35:17 -0800

    Added seperate test file for findDispatchMethodS3().

--- tests/getDispatchMethodS3.R
+++ tests/getDispatchMethodS3.R
@@ -12,11 +12,4 @@ tryCatch({
   print(ex)
 })
 
-
-## Odds and ends
-# Trying to retrieve base::.Options, but should be
-# detected as a non-function and return an empty result
-fcn <- findDispatchMethodsS3("", "Options")
-stopifnot(length(fcn) == 0L)
-
 message("TESTING: getDispatchMethodS3()...DONE")
