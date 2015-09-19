39d706f4cf947ef9b8a990cb949c7fa6a896cdb8
hb <hb@aroma-project.org>
2015-09-18 17:49:19 -0700

    CLEANUP: Dropped unused code

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,6 @@
 ## covr: skip=all
 
 .onAttach <- function(libname, pkgname) {
-  # Set up RCC validators for R.methodsS3
-  validators <- getOption("R.methodsS3:validators:setGenericS3", list())
-  validators <- c(validators, list(rccValidateSetGenericS3))
-  validators <- getOption("R.methodsS3:validators:setMethodS3", list())
-  validators <- c(validators, list(rccValidateSetMethodS3))
-
   pi <- utils::packageDescription(pkgname)
   pkgStartupMessage(pkgname, " v", pi$Version, " (", pi$Date,
            ") successfully loaded. See ?", pkgname, " for help.")
