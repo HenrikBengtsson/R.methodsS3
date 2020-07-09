c15e94a46a54f5138ea43d7c6295e8d374138309
Henrik Bengtsson <hb@aroma-project.org>
2020-07-09 11:27:38 -0700

    BUG FIXES: R.methodsS3 would produce "Warning: partial match of 'Date' to 'Date/Publication'" when attached

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,12 @@
 .onAttach <- function(libname, pkgname) {
   pd <- utils::packageDescription(pkgname)
   msg <- sprintf("%s v%s", pkgname, pd$Version)
-  if (!is.null(pd$Date)) msg <- sprintf("%s (%s)", msg, pd$Date)
+  
+  field <- intersect(c("Date/Publication", "Date"), names(pd))
+  if (length(field) > 0L) {
+    date <- pd[[field[1]]]
+    if (!is.na(date)) msg <- sprintf("%s (%s)", msg, date)
+  }
   msg <- sprintf("%s successfully loaded. See ?%s for help.", msg, pkgname)
   pkgStartupMessage(msg)
 }
